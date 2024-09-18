571ea3c20c6a75c1b596de9ccca5902df8eb65fb
Nicolas <ncorantehuenchuman@gmail.com>
2024-09-18 14:29:00 -0300

    Traer datos de a API

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
-<script setup>
+<script>
+import axios from 'axios';
+import PokemonCard from './components/PokemonCard.vue';
+export default {
+
+  data() {
+    return {
+      pokemons: [],
+    }
+  },
+  components: {
+    PokemonCard
+  },
+  methods: {
+    async getPokemons() {
+      const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10');
+      this.pokemons = await Promise.all(data.results.map(async (poke) => {
+        const { data: pokemonData } = await axios.get(poke.url);
+        return {
+          id: pokemonData.id,
+          name: pokemonData.name,
+          image: pokemonData.sprites.front_default,
+        };
+      }));
+      console.log(this.pokemons);
+    }
+  },
+  mounted() {
+    this.getPokemons();
+  }
+}
+
 </script>
 
 <template>
+  <PokemonCard />
+  <p v-for="pokemon in pokemons">{{ pokemon.name }}</p>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
